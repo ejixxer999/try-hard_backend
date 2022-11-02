078ec449833f5017365debbe46bacc29db019ed2
ejixxer999 <lehman.ethan2@gmail.com>
2022-11-01 21:45:59 -0400

    created Users session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,5 +9,22 @@ class SessionsController < ApplicationController
         end
     end
 
-    def 
+    def google_login
+        user_email = request.env['omniauth.auth']['info']['email']
+        user_name = request.env['omniauth.auth']['info']['name']
+        user = User.find_or_create_by(email: user_email) do |user|
+            user.username = user_name
+            user.password = SecureRandom.hex
+        end
+        session[:user_id] = user.id
+
+        redirect_to user_path(user)
+
+    end 
+
+    def destroy
+        logout
+        redirect_to '/'
+    end 
+
 end
