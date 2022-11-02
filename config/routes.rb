c3f172fc2471ba767ebe4e1f1e6fd3eb98f33a7a
ejixxer999 <lehman.ethan2@gmail.com>
2022-11-01 22:04:44 -0400

    added custom routes for user login and interaction

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+
+
+  get'/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  delete '/logout' => 'sessions#destroy'
+  get '/auth/google_oauth2/callback' => 'sessions#google_login'
   resources :catagories
   resources :products
   resources :users
@@ -6,5 +12,9 @@ Rails.application.routes.draw do
   resources :catagories do 
     resources :products
   end
+
+  resources :user do 
+    resources :products, only:[:index, :show]
+    resource :catagories, only:[:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
