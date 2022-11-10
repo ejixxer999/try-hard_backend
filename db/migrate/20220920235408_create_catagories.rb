74a0bfe7615aa1748652f0bb1b379c4c5bdf869d
ejixxer999 <lehman.ethan2@gmail.com>
2022-11-09 22:18:46 -0500

    added product id for catagories

--- db/migrate/20220920235408_create_catagories.rb
+++ db/migrate/20220920235408_create_catagories.rb
@@ -2,6 +2,8 @@ class CreateCatagories < ActiveRecord::Migration[6.1]
   def change
     create_table :catagories do |t|
       t.string :name
+      t.integer :product_id
+      
 
       t.timestamps
     end
