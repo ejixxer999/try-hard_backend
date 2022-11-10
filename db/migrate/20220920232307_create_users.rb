848e70d3b0584decda93ffe2add67f57e087097e
ejixxer999 <lehman.ethan2@gmail.com>
2022-11-09 22:19:24 -0500

    revised user attributes

--- db/migrate/20220920232307_create_users.rb
+++ db/migrate/20220920232307_create_users.rb
@@ -3,7 +3,10 @@ class CreateUsers < ActiveRecord::Migration[6.1]
     create_table :users do |t|
       t.string :username
       t.string :email
-      t.string :uid
+      t.string :password_digest
+      t.string :provider
+      t.string :address
+      t.integer :credit_card
       
 
       t.timestamps
