d40413c26ba863deaab590f3e26f452fbc624d35
ejixxer999 <lehman.ethan2@gmail.com>
2022-11-16 20:19:43 -0500

    created home component

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -2,7 +2,8 @@ import React from "react"
 
 function Home(){
     return(
-        <div>
+        <div id="home">
+            <h1 style ={{ color: "white", width: "300px", fontsize: "35px"}}>Fresh Look Details</h1>
 
         </div>
     )
