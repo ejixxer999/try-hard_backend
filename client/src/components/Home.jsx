561e44e1c3b1bea5f75ad4cf68a9bfe3c47d0b98
ejixxer999 <lehman.ethan2@gmail.com>
2022-12-08 21:46:09 -0500

    repurposing the home page

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,11 +1,14 @@
 import React from "react"
 
+
+
+
 function Home(){
     return(
-        <div id="home">
-            <h1 style ={{ color: "white", width: "300px", fontsize: "35px"}}>Fresh Look Details</h1>
+       <div>
 
-        </div>
+       </div>
+        
     )
 }
 
