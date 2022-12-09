9af5398fa5a228343eb0ee875cd6cbb76238a6ac
ejixxer999 <lehman.ethan2@gmail.com>
2022-12-08 21:48:40 -0500

    config news slider on home page

---
+++ client/src/components/news/index.js
@@ -0,0 +1,55 @@
+import { Slide } from "@mui/material";
+import { Box } from "@mui/system";
+import { useEffect, useState } from "react";
+import { Messages, NewsContainer } from "../../styles/News";
+
+
+const messages = [
+    "This is where all of the announments to the customers will go",
+    "dates we are open or closed",
+    "new products we are selling",
+    "discounts and sales"
+]
+
+function News() {
+    const [messageIndex, setMessageIndex] = useState(0)
+    const [show, setShow] = useState(true)
+
+
+    useEffect(() => {
+        setTimeout(() => {
+            setShow(false)
+        }, 3000)
+        const intervalId = setInterval(() => {
+            setMessageIndex(i => (i + 1) % messages.length)
+            setShow(true)
+
+            setTimeout(() => {
+                setShow(false)
+            }, 3000)
+
+        }, 4000)
+        return () => {
+            clearInterval(intervalId);
+        }
+    },[])
+
+
+    return(
+       <NewsContainer>
+        <Slide direction={show ? "right": "left"} in={show} timeout={{
+            enter: 500,
+            exit:100
+        }}>
+        <Box display={"flex"} justifyContent="center" alignItems={"center"}>
+            <Messages>
+                {messages[messageIndex]}
+            </Messages>
+        </Box>
+        </Slide>
+       </NewsContainer>
+    )
+
+}
+
+export default News;
