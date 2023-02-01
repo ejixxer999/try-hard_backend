3a535a3efa97f03f660c2229256f71d499b4e075
ejixxer999 <lehman.ethan2@gmail.com>
2023-01-31 22:09:36 -0500

    created mailer

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import emailjs from "emailjs-com"
 
 function Contact() {
+  const form = useRef()
+
+  const sendEmail = (e) => {
+    e.preventDefault()
+
+    emailjs.sendForm('gmail_service', 'contact_form', form.current, 'bEafiKe74vPjQlfRp')
+    .then((result) => {
+      console.log(result.text)
+    }, (error) => {
+      console.log(error.text)
+    })
+  }
+
   return (
-    <div>Contact</div>
+    <form ref={form} onSubmit={sendEmail}>
+      <label>Name</label>
+      <input type="text" name="user_name" />
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <label>Message</label>
+      <textarea name="message" />
+      <input type="submit" value="Send" />
+    </form>
   )
 }
 
